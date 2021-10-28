9d60a8f5d3a15c63c81a3f67bfd6f77add79f316
Sergio200x <sergio_200x@hotmail.com>
2021-10-27 23:31:12 -0300

    trabajo en clase

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
  const express = require("express");
  const app = express();
+ const path = require('path')
+ const port =3030
+ app.use(express.static("public"));
 
- console.log(express)
+ app.get("/home", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/index.html")))
+
+ app.get("/", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/index.html")))
+
+ app.listen(port,()=>{console.log("servidor activo")})
+
+
+
+
+
+
+ 
